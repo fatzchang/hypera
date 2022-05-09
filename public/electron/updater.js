d511a3e57a62efa374127c7785fe98076cc6937e
fatzchang <fatz.zhang@gmail.com>
2022-05-09 22:35:47 +0800

    ask user to update

--- public/electron/updater.js
+++ public/electron/updater.js
@@ -1,7 +1,40 @@
+const { dialog } = require('electron');
 const { autoUpdater } = require('electron-updater');
 autoUpdater.logger = require('electron-log');
 autoUpdater.logger.transports.file.level = 'info';
 
+autoUpdater.autoDownload = false;
+
 module.exports = () => {
-  autoUpdater.checkForUpdatesAndNotify();
+  autoUpdater.checkForUpdates();
+
+  autoUpdater.on('update-available', () => {
+    dialog.showMessageBox({
+      type: 'info',
+      title: 'Update available',
+      message: 'A new version of Hypera is available. Do you want to update now?',
+      buttons: ['Update', 'Cancel']
+    }).then(result => {
+      let buttonIndex = result.response;
+
+      if (buttonIndex === 0) {
+        autoUpdater.downloadUpdate();
+      }
+    });
+  });
+
+  autoUpdater.on('update-downloaded', () => {
+    dialog.showMessageBox({
+      type: 'info',
+      title: 'Update ready',
+      message: 'New version is downloaded. Do you want to install & restart now?',
+      button: ['Yes', 'Later']
+    }).then(result => {
+      let buttonIndex = result.response;
+
+      if (buttonIndex === 0) {
+        autoUpdater.quitAndInstall(false, true);
+      }
+    });
+  })
 }
